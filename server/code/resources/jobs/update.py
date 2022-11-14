04b4b0ecbdc8febc6f3fddd4e2f8717057cd85e8
lhkhoi95 <lhkhoi95@gmail.com>
2022-11-14 11:30:19 -0800

    fix update job

--- server/code/resources/jobs/update.py
+++ server/code/resources/jobs/update.py
@@ -47,13 +47,10 @@ class UpdateJob(Resource):
             abort(400, message='start_date cannot be greater than end_date')
 
         # Find job by job_id
-        job = JobModel.find_one_job_company_application(data['job_id'])
+        job = JobModel.find_by_job_id(data['job_id'])
 
         user_id = get_jwt_identity().get('user_id')
-
-        # Find all jobs that current candidate has posted
-        jobs = JobModel.find_all_job_company_by_uid(user_id)
-        if job not in jobs:
+        if job is None or job.user_id != user_id:
             abort(403, message='You are not authorized to update this job')
 
         try:
